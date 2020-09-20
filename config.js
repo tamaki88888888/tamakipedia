777d33e61fddbd9c4554316253080ac940c7f879
tamaki888888 <koukoukokoko@icloud.com>
2020-09-20 16:55:05 +0900

    change title tamakipedia

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
+  title: 'tamakipedia',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
